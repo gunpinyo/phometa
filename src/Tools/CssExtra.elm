3b4dd524e03cc54847c8f22b0ce88d1e78885162
Gun Pinyo <gunpinyo@gmail.com>
2016-02-27 15:38:36 +0000

    Forget to add some file on last commit, so do it here

---
+++ src/Tools/CssExtra.elm
@@ -0,0 +1,59 @@
+module Tools.CssExtra where
+
+import String
+import Debug
+
+import Html exposing (Html, text, div)
+import Html.Attributes exposing (class)
+
+import Tools.HtmlExtra exposing (debug_to_html)
+
+type alias CssKey = String
+type alias CssValue = String
+type alias CssStyle = List (CssKey, CssValue)
+type alias CssClass = String
+
+type alias CssColor = String
+type alias CssSize = String
+
+type alias CssInlineStr = String
+type CssInlineToken
+  = CssInlineTokenText String
+  | CssInlineTokenOpen CssClass
+  | CssInlineTokenClose
+
+css_inline_str_delimiter : String
+css_inline_str_delimiter = "⁑"
+
+css_inline_str_embed : CssClass -> CssInlineStr -> CssInlineStr
+css_inline_str_embed css_class string =
+  css_inline_str_delimiter ++ css_class ++ css_inline_str_delimiter
+    ++ string
+    ++ css_inline_str_delimiter ++ css_inline_str_delimiter
+
+css_inline_str_compile : CssInlineStr -> List Html
+css_inline_str_compile css_inline_str =
+  let err_msg = "bug in function Tools.CssExtra.css_inline_str_complie"
+      tokens = String.split css_inline_str_delimiter css_inline_str
+                 |> List.indexedMap (\index raw_token ->
+                      if index % 2 == 0 then
+                        CssInlineTokenText raw_token
+                      else if raw_token /= "" then
+                        CssInlineTokenOpen raw_token
+                      else
+                        CssInlineTokenClose)
+      -- this function operate with reverse order
+      -- this play well with list representation
+      func token stack = case (token, stack) of
+        (CssInlineTokenText string, htmls :: stack') ->
+          ((Html.text string) :: htmls) :: stack'
+        (CssInlineTokenOpen css_class, htmls' :: htmls :: stack') ->
+          ((div [class css_class] htmls') :: htmls) :: stack'
+        (CssInlineTokenClose, _) ->
+          [] :: stack
+        _ -> Debug.crash err_msg
+   in List.foldr func [[]] tokens
+        |> List.concat -- we know that that this stage
+                       -- stack will have one element which is result
+                       -- we want to lift this result to top level
+                       -- we can do this by List.concat
---
+++ src/Tools/StripedList.elm
@@ -0,0 +1,36 @@
+module Tools.StripedList where
+
+import Debug
+
+-- represent a list that always has odd length
+-- with odd element has type a and even element has type b
+type StripedList a b
+  = StripedListNil a
+  | StripedListCons b a (StripedList a b)
+
+striped_list_introduce : List a -> List b -> StripedList a b
+striped_list_introduce list_a list_b =
+  case (list_a, list_b) of
+    (a :: list_a', b :: list_b') ->
+      StripedListCons b a (striped_list_introduce list_a' list_b')
+    ([a], []) -> StripedListNil a
+    _ -> Debug.crash "impossible input from function striped_list_introduce"
+
+striped_list_eliminate : (a -> c) -> (b -> c) -> StripedList a b -> List c
+striped_list_eliminate func_a func_b list =
+  case list of
+    StripedListNil a -> [func_a a]
+    StripedListCons b a list' ->
+      func_b b :: func_a a :: striped_list_eliminate func_a func_b list'
+
+striped_list_get_even_element : StripedList a b -> List a
+striped_list_get_even_element list =
+  case list of
+    StripedListNil a -> [a]
+    StripedListCons b a list' -> a :: striped_list_get_even_element list'
+
+striped_list_get_odd_element : StripedList a b -> List b
+striped_list_get_odd_element list =
+  case list of
+    StripedListNil a -> []
+    StripedListCons b a list' -> b :: striped_list_get_odd_element list'
